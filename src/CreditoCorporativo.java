55be295b5a64bf6bbdb09aed0a06ea764ae57c65
andersonsua0331-rgb <andersonsua0331@gmail.com>
2026-02-04 00:51:59 +0000

    creacion de clase corporativa con atrivutos

--- src/CreditoCorporativo.java
+++ src/CreditoCorporativo.java
@@ -1,1 +1,10 @@
+import java.lang.String;
 
+public class CreditoCorporativo{
+    private String id;
+    private double montoPrestado;
+    private double saldoPendiente;
+    private double tasaInteres;
+    private boolean esRiengoso;
+
+}
