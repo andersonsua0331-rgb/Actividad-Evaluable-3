3133843a673ea019fd78c75b88b968788cb50cf2
andersonsua0331-rgb <andersonsua0331@gmail.com>
2026-02-07 00:24:18 +0000

    cambios de CreditoCorporativo

--- src/CreditoCorporativo.java
+++ src/CreditoCorporativo.java
@@ -1,10 +1,42 @@
 import java.lang.String;
 
 public class CreditoCorporativo{
-    private String id;
+    private String Id;
     private double montoPrestado;
     private double saldoPendiente;
     private double tasaInteres;
-    private boolean esRiengoso;
+    private boolean esRiesgoso; 
 
+
+    public CreditoCorporativo(String Id, double montoPrestado, double tasaInteres){
+         this.Id=Id;
+        this.montoPrestado=montoPrestado;
+        this.saldoPendiente=saldoPendiente;
+        this.tasaInteres=tasaInteres;
+        this.esRiesgoso=false;
+    }
+    public CreditoCorporativo(String Id, double montoPrestado, double saldoPendiente,double tasaInteres){
+        this.Id=Id;
+        this.montoPrestado=montoPrestado;
+        this.saldoPendiente=saldoPendiente;
+        this.tasaInteres=tasaInteres;
+        this.esRiesgoso=false;
+    }
+    public CreditoCorporativo(String Id, double montoPrestado){
+        this.Id=Id;
+        this.montoPrestado=montoPrestado;
+        this.saldoPendiente=saldoPendiente;
+        this.tasaInteres=1.5;
+        this.esRiesgoso=false;
+    }
+    public void Setsaldopendiente(double saldoPendiente){
+        if (saldoPendiente<0){
+            System.out.println("error");
+        }else{
+            this.saldoPendiente= saldoPendiente;
+        }
+    }
+
+    
+ 
 }
